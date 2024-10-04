75289074c872041361d9e3080826fce904b58a2c
Cosmin Spanu <cosminspanu03@gmail.com>
2024-10-04 20:42:11 +0300

    Rewritten comments on EDA file

--- EDA.sql
+++ EDA.sql
@@ -1,3 +1,4 @@
+--GENERAL ANALYSIS
 CREATE VIEW categorical_counts
 AS
 SELECT COUNT(*) AS Total, COUNT(DISTINCT(Sales.game_name)) AS game_names, 
@@ -11,8 +12,6 @@ JOIN Games
 ON Sales.game_name = Games.game_name
 AND Sales.platform = Games.platform
 AND Sales.year_of_release = Games.year_of_release;
-
---Value with the highest frequency for categorical variables
 
 CREATE VIEW categorical_mode_values
 AS
@@ -94,24 +93,50 @@ SELECT MIN(global_sales),
 	ROUND(STDDEV_POP(global_sales), 2) AS standard_deviation
 FROM Sales;
 
---Which are the games that sold the best in every part of the world?
-SELECT game_name, platform, year_of_release, na_sales FROM Sales
-WHERE na_sales = (SELECT MAX(na_sales) FROM Sales);
+--What is the game that has the most versions?
+SELECT game_name, COUNT(*) AS version_num FROM Games
+GROUP BY game_name
+ORDER BY version_num DESC
+LIMIT 1;
 
-SELECT game_name, platform, year_of_release, eu_sales FROM Sales
-WHERE eu_sales = (SELECT MAX(eu_sales) FROM Sales);
+--Which developer has the best Sales/volume ratio?
+--The condition COUNT(*) > 1 was added so that no developer with only one game would skew the result
+SELECT developer, ROUND(SUM(global_sales) / COUNT(*), 2) AS SalesToVolumeRatio FROM Developers dev
+JOIN Sales s
+ON s.game_version_id = dev.game_version_id
+GROUP BY developer
+HAVING COUNT(*) > 1
+ORDER BY SalesToVolumeRatio DESC
+LIMIT 1;
 
-SELECT game_name, platform, year_of_release, jp_sales FROM Sales
-WHERE jp_sales = (SELECT MAX(jp_sales) FROM Sales);
+--Do ratings geared towards a younger demographic sell more?
+SELECT rating, SUM(global_sales) AS total_sales FROM Games
+JOIN Sales
+ON Games.game_name = Sales.game_name
+AND Games.platform = Sales.platform
+AND Games.year_of_release = Sales.year_of_release
+GROUP BY rating
+ORDER BY total_sales DESC;
 
-SELECT game_name, platform, year_of_release, other_sales FROM Sales
-WHERE other_sales = (SELECT MAX(other_sales) FROM Sales);
 
-SELECT game_name, platform, year_of_release, global_sales FROM Sales
-WHERE global_sales = (SELECT MAX(global_sales) FROM Sales);
+--REGION-BASED ANALYSIS
+--What are the games with the highest sales for each region?
+(SELECT 'NA', game_name, platform, year_of_release, na_sales AS sales FROM Sales
+WHERE na_sales = (SELECT MAX(na_sales) FROM Sales))
+UNION
+(SELECT 'EU', game_name, platform, year_of_release, eu_sales AS sales FROM Sales
+WHERE eu_sales = (SELECT MAX(eu_sales) FROM Sales))
+UNION
+(SELECT 'JP', game_name, platform, year_of_release, jp_sales AS sales FROM Sales
+WHERE jp_sales = (SELECT MAX(jp_sales) FROM Sales))
+UNION
+(SELECT 'Other', game_name, platform, year_of_release, other_sales AS sales FROM Sales
+WHERE other_sales = (SELECT MAX(other_sales) FROM Sales))
+UNION
+(SELECT 'Global', game_name, platform, year_of_release, global_sales AS sales FROM Sales
+WHERE global_sales = (SELECT MAX(global_sales) FROM Sales));
 
---Is there a difference between the prefferences of genre from one region to another? 
-	--How about rating?	
+--Is there a difference in the prefferences of genre from one region to another?
 SELECT genre, SUM(na_sales) AS sales FROM Games
 JOIN Sales
 ON Games.game_name = Sales.game_name
@@ -149,175 +174,88 @@ ORDER BY SUM(other_sales) DESC
 LIMIT 3;
 
 --Which publihser had the best sales in each region?
-SELECT publisher, SUM(na_sales) AS sales FROM Games
+(
+SELECT 'NA', publisher, SUM(na_sales) AS sales FROM Games
 JOIN Sales
 ON Games.game_name = Sales.game_name
 AND Games.platform = Sales.platform
 AND Games.year_of_release = Sales.year_of_release
 GROUP BY publisher
 ORDER BY SUM(na_sales) DESC
-LIMIT 1;
-
-SELECT publisher, SUM(eu_sales) AS sales FROM Games
+LIMIT 1
+)
+UNION
+(
+SELECT 'EU', publisher, SUM(eu_sales) AS sales FROM Games
 JOIN Sales
 ON Games.game_name = Sales.game_name
 AND Games.platform = Sales.platform
 AND Games.year_of_release = Sales.year_of_release
 GROUP BY publisher
 ORDER BY SUM(eu_sales) DESC
-LIMIT 1;
-
-SELECT publisher, SUM(jp_sales) AS sales FROM Games
+LIMIT 1
+)
+UNION
+(
+SELECT 'JP', publisher, SUM(jp_sales) AS sales FROM Games
 JOIN Sales
 ON Games.game_name = Sales.game_name
 AND Games.platform = Sales.platform
 AND Games.year_of_release = Sales.year_of_release
 GROUP BY publisher
 ORDER BY SUM(jp_sales) DESC
-LIMIT 1;
-
-SELECT publisher, SUM(other_sales) AS sales FROM Games
+LIMIT 1
+)
+UNION
+(
+SELECT 'Other', publisher, SUM(other_sales) AS sales FROM Games
 JOIN Sales
 ON Games.game_name = Sales.game_name
 AND Games.platform = Sales.platform
 AND Games.year_of_release = Sales.year_of_release
 GROUP BY publisher
 ORDER BY SUM(other_sales) DESC
-LIMIT 1;
-
---Which developer had the best sales in each region?
-SELECT developer, SUM(na_sales) AS sales FROM Developers dev
-JOIN Sales
-ON dev.game_version_id = Sales.game_version_id
-GROUP BY developer 
-ORDER BY SUM(na_sales) DESC
-LIMIT 1;
-
-SELECT developer, SUM(eu_sales) AS sales FROM Developers dev
-JOIN Sales
-ON dev.game_version_id = Sales.game_version_id
-GROUP BY developer 
-ORDER BY SUM(eu_sales) DESC
-LIMIT 1;
-
-SELECT developer, SUM(jp_sales) AS sales FROM Developers dev
-JOIN Sales
-ON dev.game_version_id = Sales.game_version_id
-GROUP BY developer 
-ORDER BY SUM(jp_sales) DESC
-LIMIT 1;
-
-SELECT developer, SUM(other_sales) AS sales FROM Developers dev
-JOIN Sales
-ON dev.game_version_id = Sales.game_version_id
-GROUP BY developer 
-ORDER BY SUM(other_sales) DESC
-LIMIT 1;
-
---Which region has the most sales?
-SELECT SUM(na_sales) AS NA, SUM(eu_sales) AS EU, SUM(jp_sales) AS JP, SUM(other_sales) AS Other
-FROM Sales;
-
---Which is the most succesful year for the video game industry?
-SELECT year_of_release, SUM(global_sales) FROM Sales
-GROUP BY year_of_release
-ORDER BY SUM(global_sales) DESC
-LIMIT 1;
-
---What's the time period of the data and how far is it from today?
-SELECT MIN(year_of_release) AS low_limit, MAX(year_of_release) AS high_limit,
-	EXTRACT(YEAR FROM current_timestamp) - MAX(year_of_release) AS diff_from_current_day
-FROM Sales;
-
---What are the sales of every platform in each of the years that they had games in?
-SELECT platform,year_of_release, SUM(global_sales) AS Sales FROM Sales
-GROUP BY CUBE(platform, year_of_release)
-ORDER BY year_of_release;
-
---Calculate the year_over_year sales growth for each paltform
-WITH Agg_year_platform_sales AS
-(
-	SELECT platform, year_of_release, SUM(global_sales) AS Sales FROM Sales
-	GROUP BY platform, year_of_release
-)
-SELECT *, LAG(sales) OVER(PARTITION BY platform ORDER BY year_of_release) AS previous_sales,
-	sales - LAG(sales) OVER(PARTITION BY platform ORDER BY year_of_release) AS Yoy_growth
-FROM Agg_year_platform_sales;
-
---Rank games by their sales within each release year
-WITH Agg_year_game_sales AS
-(
-	SELECT game_name, year_of_release, SUM(global_sales) AS sales FROM Sales
-	GROUP BY year_of_release, game_name
-)
-SELECT *, RANK() OVER(PARTITION BY year_of_release ORDER BY sales DESC)
-FROM Agg_year_game_sales;
-
---Calculate the trailing 3-year average sales for all the developers that released a game in the 
---most recent year(lag)
-WITH developers_recent_sales AS
-(
-	SELECT developer, year_of_release, Sales as Y1,
-		LAG(Sales) OVER(PARTITION BY developer ORDER BY year_of_release) AS Y2,
-		LAG(Sales, 2) OVER(PARTITION BY developer ORDER BY year_of_release) AS Y3
-	FROM(
-		SELECT developer, year_of_release, SUM(global_sales) as Sales FROM Developers d
-		JOIN Sales s
-		ON d.game_version_id = s.game_version_id
-		WHERE developer IN
-		(
-			SELECT DISTINCT(developer) FROM Developers dev
-			JOIN Sales s
-			ON dev.game_version_id = s.game_version_id
-			WHERE year_of_release = 2016
-		)
-		GROUP BY developer, year_of_release
-		ORDER BY developer, year_of_release
-	)
-)
-SELECT developer, ROUND((y1 + y2 + y3) / 3, 2) AS Trailing_Sales_3Y FROM developers_recent_sales
-WHERE year_of_release = 2016
-AND y2 IS NOT NULL
-AND y3 IS NOT NULL
-ORDER BY Trailing_Sales_3Y DESC
-
---Identify the publishers and years in which they had both a 3 year streak of growing or declining 
---sales
-WITH publishers_recent_sales AS
-(
-	SELECT publisher, year_of_release, Sales AS Y1,
-		LAG(Sales) OVER(PARTITION BY publisher ORDER BY year_of_release) AS Y2,
-		LAG(Sales, 2) OVER(PARTITION BY publisher ORDER BY year_of_release) AS Y3
-	FROM (
-		SELECT publisher, s.year_of_release, SUM(global_sales) AS sales FROM Sales s
-		JOIN Games gm
-		ON s.game_name = gm.game_name
-		AND s.platform = gm.platform
-		AND s.year_of_release = gm.year_of_release
-		GROUP BY publisher, s.year_of_release
-		ORDER BY publisher ASC, s.year_of_release DESC
-	)
-)
-SELECT publisher, year_of_release,
-	CASE
-		WHEN Y1 > Y2 AND Y2 > Y3 THEN 'Growing'
-		WHEN Y1 < Y2 AND Y2 < Y3 THEN 'Declining'
-	END
-FROM publishers_recent_sales
-WHERE (Y1 > Y2 AND Y2 > Y3)
-OR (Y1 < Y2 AND Y2 < Y3);
-
---Rank each platform by total global sales
-SELECT platform, RANK()OVER(ORDER BY total_sales DESC)
-FROM
-(
-	SELECT platform, SUM(global_sales) AS total_sales
-	FROM Sales
-	GROUP BY platform
+LIMIT 1
 );
 
---Are there platform prefferences when it comes to geographical allocation? If so, which platform
---	supported the most sales in which region?
+--Which developer had the best sales in each region?
+(
+SELECT 'NA', developer, SUM(na_sales) AS sales FROM Developers dev
+JOIN Sales
+ON dev.game_version_id = Sales.game_version_id
+GROUP BY developer 
+ORDER BY SUM(na_sales) DESC
+LIMIT 1
+)
+UNION
+(
+SELECT 'EU', developer, SUM(eu_sales) AS sales FROM Developers dev
+JOIN Sales
+ON dev.game_version_id = Sales.game_version_id
+GROUP BY developer 
+ORDER BY SUM(eu_sales) DESC
+LIMIT 1
+)
+UNION
+(
+SELECT 'JP', developer, SUM(jp_sales) AS sales FROM Developers dev
+JOIN Sales
+ON dev.game_version_id = Sales.game_version_id
+GROUP BY developer 
+ORDER BY SUM(jp_sales) DESC
+LIMIT 1
+)
+UNION
+(
+SELECT 'Other', developer, SUM(other_sales) AS sales FROM Developers dev
+JOIN Sales
+ON dev.game_version_id = Sales.game_version_id
+GROUP BY developer 
+ORDER BY SUM(other_sales) DESC
+LIMIT 1
+);
+
+--Are there platform prefferences when it comes to geographical allocation?
 (
 	SELECT 'Na', platform FROM Sales
 	GROUP BY platform
@@ -346,7 +284,122 @@ UNION
 	LIMIT 1
 );
 
---What is the top 3 best selling games for each platform?
+--Which region has the most sales?
+SELECT SUM(na_sales) AS NA, SUM(eu_sales) AS EU, SUM(jp_sales) AS JP, SUM(other_sales) AS Other
+FROM Sales;
+
+
+--TIME-BASED ANALYSIS
+--Which is the most succesful year for the video game industry?
+SELECT year_of_release, SUM(global_sales) FROM Sales
+GROUP BY year_of_release
+ORDER BY SUM(global_sales) DESC
+LIMIT 1;
+
+--What's the time period of the data and how far is it from today?
+SELECT MIN(year_of_release) AS low_limit, MAX(year_of_release) AS high_limit,
+	EXTRACT(YEAR FROM current_timestamp) - MAX(year_of_release) AS diff_from_current_day
+FROM Sales;
+
+--What are the sales of every platform in each of the years that they had games in?
+SELECT year_of_release, platform, SUM(global_sales) AS Sales FROM Sales
+GROUP BY CUBE(platform, year_of_release)
+ORDER BY year_of_release;
+
+--Calculate the year_over_year sales growth for each paltform
+--A CTE was used to allow the LAG window function to process aggregated data.
+WITH Agg_year_platform_sales AS
+(
+	SELECT platform, year_of_release, SUM(global_sales) AS Sales FROM Sales
+	GROUP BY platform, year_of_release
+)
+SELECT *, LAG(sales) OVER(PARTITION BY platform ORDER BY year_of_release) AS previous_sales,
+	sales - LAG(sales) OVER(PARTITION BY platform ORDER BY year_of_release) AS Yoy_growth
+FROM Agg_year_platform_sales;
+
+--Rank games by their sales within each release year
+WITH Agg_year_game_sales AS
+(
+	SELECT game_name, year_of_release, SUM(global_sales) AS sales FROM Sales
+	GROUP BY year_of_release, game_name
+)
+SELECT *, RANK() OVER(PARTITION BY year_of_release ORDER BY sales DESC)
+FROM Agg_year_game_sales;
+
+/*
+Calculate the trailing 3-year average sales for all the developers that released a game in the 
+most recent year. 
+I'll assume that the most recent year is 2016 as there is only 2 games released
+in 2017 in this dataset.
+The CTE contains the total sales of the 3 most recent years of the developers that have a game in
+2016
+*/
+WITH developers_recent_sales AS
+(
+	SELECT developer, year_of_release, Sales as Y1,
+		LAG(Sales) OVER(PARTITION BY developer ORDER BY year_of_release) AS Y2,
+		LAG(Sales, 2) OVER(PARTITION BY developer ORDER BY year_of_release) AS Y3
+	FROM(
+		SELECT developer, year_of_release, SUM(global_sales) as Sales FROM Developers d
+		JOIN Sales s
+		ON d.game_version_id = s.game_version_id
+		WHERE developer IN
+		(
+			SELECT DISTINCT(developer) FROM Developers dev
+			JOIN Sales s
+			ON dev.game_version_id = s.game_version_id
+			WHERE year_of_release = 2016
+		)
+		GROUP BY developer, year_of_release
+		ORDER BY developer, year_of_release
+	)
+)
+SELECT developer, ROUND((y1 + y2 + y3) / 3, 2) AS Trailing_Sales_3Y FROM developers_recent_sales
+WHERE year_of_release = 2016
+AND y2 IS NOT NULL
+AND y3 IS NOT NULL
+ORDER BY Trailing_Sales_3Y DESC
+
+/*
+List the publishers that experienced three consecutive years of either increasing or decreasing 
+sales, along with the respective years.
+*/
+WITH publishers_recent_sales AS
+(
+	SELECT publisher, year_of_release, Sales AS Y1,
+		LAG(Sales) OVER(PARTITION BY publisher ORDER BY year_of_release) AS Y2,
+		LAG(Sales, 2) OVER(PARTITION BY publisher ORDER BY year_of_release) AS Y3
+	FROM (
+		SELECT publisher, s.year_of_release, SUM(global_sales) AS sales FROM Sales s
+		JOIN Games gm
+		ON s.game_name = gm.game_name
+		AND s.platform = gm.platform
+		AND s.year_of_release = gm.year_of_release
+		GROUP BY publisher, s.year_of_release
+		ORDER BY publisher ASC, s.year_of_release DESC
+	)
+)
+SELECT publisher, year_of_release,
+	CASE
+		WHEN Y1 > Y2 AND Y2 > Y3 THEN 'Growing'
+		WHEN Y1 < Y2 AND Y2 < Y3 THEN 'Declining'
+	END
+FROM publishers_recent_sales
+WHERE (Y1 > Y2 AND Y2 > Y3)
+OR (Y1 < Y2 AND Y2 < Y3);
+
+
+--PLATFORM ANALYSIS
+--Rank each platform by total global sales
+SELECT platform, RANK()OVER(ORDER BY total_sales DESC)
+FROM
+(
+	SELECT platform, SUM(global_sales) AS total_sales
+	FROM Sales
+	GROUP BY platform
+);
+
+--What are the top 3 best selling games for each platform?
 SELECT platform, game_name, rnk
 FROM
 (
@@ -357,14 +410,8 @@ FROM
 WHERE rnk <= 3
 ORDER BY platform, rnk;
 
---Which developer has the best Sales/volume ratio?
-SELECT developer, ROUND(SUM(global_sales) / COUNT(*), 2) AS SalesToVolumeRatio FROM Developers dev
-JOIN Sales s
-ON s.game_version_id = dev.game_version_id
-GROUP BY developer
-ORDER BY SalesToVolumeRatio DESC
-LIMIT 1;
 
+--PUBLISHERS ANALYSIS
 --Which publisher has the best sale/volume ratio?
 SELECT publisher, ROUND(SUM(global_sales) / COUNT(*), 2) AS SalesVolumeRatio FROM Games 
 JOIN Sales
@@ -372,10 +419,11 @@ ON Sales.game_name = Games.game_name
 AND Sales.platform = Games.platform
 AND Sales.year_of_release = Games.year_of_release
 GROUP BY publisher
+HAVING COUNT(*) > 1
 ORDER BY SalesVolumeRatio DESC
 LIMIT 1;
 
---Identify the highest and second highest selling games for each publisher(rank)
+--Identify the highest and second highest selling games for each publisher
 SELECT publisher, game_name, platform, year_of_release, rnk
 FROM
 (
@@ -389,7 +437,12 @@ FROM
 )
 WHERE rnk < 3;
 
---Identify the first game by each publisher to reach 1 million in global sales(row_number)
+--Identify the first game by each publisher to reach 1 million in global sales
+/*
+The query uses the row number function on a table of all the games that have more than 1 million 
+monetary units in global sales sorted by the year of release so that the tuples with the row number
+equal to one would represent the first game of the publishers to reach 1 million in sales.
+*/
 SELECT publisher, game_name, platform, year_of_release
 FROM
 (
@@ -408,31 +461,3 @@ FROM
 	)
 )
 WHERE rn = 1;
-
---What is the game that has the most versions?
-SELECT game_name, COUNT(*) AS version_num FROM Games
-GROUP BY game_name
-ORDER BY version_num DESC
-LIMIT 1;
-
---Rank games within each genre based on their sales in North America(rank or dense_rank)
-SELECT genre, game_name, platform, year_of_release, rnk FROM
-(
-	SELECT genre, Games.game_name, Games.platform, Games.year_of_release, 
-		DENSE_RANK() OVER(PARTITION BY genre ORDER BY na_sales DESC) AS rnk FROM Games
-	JOIN Sales
-	ON Sales.game_name = Games.game_name
-	AND Sales.platform = Games.platform
-	AND Sales.year_of_release = Games.year_of_release
-	ORDER BY genre, na_sales DESC
-)
-WHERE rnk <= 3;
-
---Do ratings geared towards a younger demographic sell more?
-SELECT rating, SUM(global_sales) AS total_sales FROM Games
-JOIN Sales
-ON Games.game_name = Sales.game_name
-AND Games.platform = Sales.platform
-AND Games.year_of_release = Sales.year_of_release
-GROUP BY rating
-ORDER BY total_sales DESC;
