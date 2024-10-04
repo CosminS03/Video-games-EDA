8b01ba62fc8acfaea916dab3c09b325ae6a6f9e8
Cosmin Spanu <cosminspanu03@gmail.com>
2024-10-04 18:57:11 +0300

    Fixed coments for cleaning file

--- Cleaning.sql
+++ Cleaning.sql
@@ -1,5 +1,4 @@
---UPDATE POSTGRESQL
---Creating the table for the data to be inserted into
+--IMPORTING THE DATASET
 CREATE TABLE Video_Games
 (
 	Game_Name TEXT,
@@ -20,11 +19,6 @@ CREATE TABLE Video_Games
 	Rating TEXT
 );
 
-/*Due to varying values which represent null in columns with different data types, another table with
-with all the columns having the text type will be created in which the data will be inserted and
-than filtered*/
-
---Creating the staging table
 CREATE TABLE Video_Games_Staging
 (
 	Game_Name TEXT,
@@ -45,21 +39,18 @@ CREATE TABLE Video_Games_Staging
 	Rating TEXT
 );
 
---Copying the csv data into the staging table
 COPY Video_Games_Staging 
 FROM 'C:\Program Files\PostgreSQL\16\data\Video_Games_Sales_as_at_22_Dec_2016.csv'
 DELIMITER ','
 CSV HEADER;
 
---Replacing the "N/A" values and empty strings with NULL
 UPDATE Video_Games_Staging
 SET Year_of_release = NULLIF(Year_of_release, 'N/A'),
 	Critic_score = NULLIF(Critic_score, ''),
 	Critic_count = NULLIF(Critic_count, ''),
 	User_score = NULLIF(User_score,''),
 	User_count = NULLIF(User_count, '');
-	
---Inserting the data from staging into the Video_Games table
+
 INSERT INTO Video_Games
 SELECT Game_Name, Platform, Year_of_release::INT, Genre, Publisher, NA_sales::DECIMAL(4, 2),
 	EU_sales::DECIMAL(4, 2), JP_sales::DECIMAL(4, 2), Other_sales::DECIMAL(4, 2), 
@@ -67,34 +58,27 @@ SELECT Game_Name, Platform, Year_of_release::INT, Genre, Publisher, NA_sales::DE
 	User_count::INT, Developer, Rating
 FROM Video_Games_Staging;
 
---Dropping the staging table
 DROP TABLE Video_Games_Staging;
 
-
---Checking every feature for null values
+--CLEANING THE DATA
 SELECT * FROM Video_Games
 WHERE game_name IS NULL;
---The rows with missing names of the games will have to be deleted
+
 DELETE FROM Video_Games
 WHERE game_name IS NULL;
 /*
-In this dataset there are rows which depict a certain game but with added details about their sales
-written in between two parenthesis at the end of the name(examples: weekly sales, US sales, old sales)
-Rows which contain weekly sales will be deleted so that the consistency of the data is maintained
+Rows with 'weekly sales' in parentheses at the end of the game_name column will be deleted to 
+maintain data consistency.
 */
 DELETE FROM Video_Games
 WHERE game_name LIKE '%eekly%';
-/*
-Rows which contain old sales will also be deleted because rows with updated sales of the same game
-exist
-*/
+
+--Rows with 'old sales' will be deleted as updated sales data for the same game is available.
 DELETE FROM Video_Games
 WHERE game_name LIKE '%(old%'
 OR game_name LIKE '%(Old%';
-/*
-Rows which specify that the sales are from all the regions will have the additional info deleted from 
-their name, so that it remains only the name of the game.
-*/
+
+--Rows with 'All regions' in the game_name column will be updated to remove this explanation
 UPDATE Video_Games
 SET game_name = SUBSTRING(game_name FROM 1 FOR POSITION('(' IN game_name) - 1)
 WHERE game_name LIKE '%(_ll _egion_%';
@@ -115,10 +99,8 @@ WHERE game_name IN(
 	ON t1.platform = t2.platform
 	AND t1.game_name = TRIM(SUBSTRING(t2.game_name FROM 1 FOR POSITION('(' IN t2.game_name)-1))
 );
-/*
-The sales of some games are divided into more rows based on their region. These rows will have all of
-their sales added up then deleted so that remains only one row for a game
-*/
+
+--All of the rows that show the sales of certain regions seperately will be added into one row
 UPDATE Video_Games AS t1
 SET na_sales = t1.na_sales + t2.na_sales,
 eu_sales = t1.eu_sales + t2.eu_sales,
@@ -143,18 +125,14 @@ WHERE CTID IN (
 	WHERE X.rn > 1
 );
 
-/*
-There still are rows with sales from certain regions which don't have other rows to complete their
-information, so they will be deleted
-*/
+--The remaining rows with specific regions sales that don't have complementary rows will be deleted
 DELETE FROM Video_Games
 WHERE RIGHT(game_name, 6) = 'sales)';
 
 
 SELECT * FROM Video_Games
 WHERE platform IS NULL;
---There are no null values for platform
-
+--There are no null values for the platform column
 
 SELECT * FROM Video_Games
 WHERE year_of_release IS NULL;
@@ -169,14 +147,13 @@ FROM (
 	WHERE year_of_release IS NOT NULL
 ) AS t2
 WHERE t1.game_name = t2.game_name;
-/*
-Some games have their year of release at the end of their names, so the years will be filled in from
-their names
-*/
+
+--Where possible, the years will be filled with the additional info from the game_name column
 UPDATE Video_Games
 SET year_of_release = RIGHT(game_name,4)::INTEGER
 WHERE RIGHT(game_name, 4) LIKE '20%'
 AND year_of_release IS NULL;
+
 /*
 The rest of the missing values will be calculated by making the average of the release years of the 
 next and previous games in the series of the one with null value. The way in which wether the game in
@@ -219,7 +196,6 @@ FROM platform_avg_years AS t2
 WHERE t1.platform = t2.platform
 AND year_of_release IS NULL;
 
-
 SELECT * FROM Video_Games
 WHERE genre IS NULL;
 
@@ -259,7 +235,6 @@ ALTER TABLE Video_Games DROP COLUMN critic_count;
 ALTER TABLE Video_Games DROP COLUMN user_score;
 ALTER TABLE Video_Games DROP COLUMN user_count;
 
-
 SELECT * FROM Video_Games
 WHERE developer IS NULL;
 /*
@@ -274,14 +249,13 @@ FROM
 	WHERE developer IS NOT NULL
 ) AS t2
 WHERE t1.game_name = t2.game_name;
+
 /*
-Some games may be sequels or part of a series. The ones with missing developers will have the values 
-replaced with the developers from the other games in their series. In the publisher column, which will
-be used for determining wether a game is in a series or not, there are 'N/A' values. These values 
-will have to be replaced or deleted in order to properly replace some of the null values from the 
-developer column
+Some games are sequels or part of a series. For those with missing developers, the developer info 
+will be filled in based on other games in the same series. The publisher column, used to identify 
+series, contains 'N/A' values that need to be replaced or removed to properly update the developer 
+column.
 */
---Replacing N/A
 UPDATE Video_Games AS t1
 SET publisher = t2.publisher
 FROM 
@@ -291,10 +265,10 @@ FROM
 ) AS t2
 WHERE t1.game_name = t2.game_name
 AND t1.publisher = 'N/A';
---Deleting rows with N/A
+
 DELETE FROM Video_Games
 WHERE publisher = 'N/A';
---Replacing the null values from the developer column
+
 WITH sorted_data AS 
 (
 	SELECT *,
@@ -325,10 +299,8 @@ SET developer = COALESCE(
 	)
 )
 WHERE developer IS NULL;
-/*
-Due to there being no other reliable way of inferring the developer, the rest of the data will be
-deleted
-*/
+
+--Since there's no other relaible way of inferring the developer, the remaining data will be deleted
 DELETE FROM Video_Games
 WHERE developer IS NULL;
 
@@ -348,10 +320,8 @@ FROM
 ) AS t2
 WHERE t1.game_name = t2.game_name
 AND t1.rating IS NULL;
-/*
-The games will be checked to see if they are part of a series and then based on that the rating will 
-be replaced
-*/
+
+--The games will be checked for series membership, and ratings will be replaced accordingly.
 WITH sorted_data AS 
 (
 	SELECT *,
@@ -383,11 +353,11 @@ SET rating = COALESCE
 	)
 )
 WHERE rating IS NULL;
+
 --The rest of the rows will be deleted
 DELETE FROM Video_Games
 WHERE rating IS NULL;
 
---remove duplicates
 DELETE FROM Video_Games
 WHERE CTID IN 
 (
@@ -398,11 +368,10 @@ WHERE CTID IN
 
 SELECT DISTINCT game_name FROM Video_Games;
 /*
-On visual inspection of the dataset, there still are values in the game_name column that have 
-additional informtaion inside parenthesis. These will be updated as to not have the additional info
-anymore. Exceptions are the games Dance Dance Revolution and World Soccer Winning Eleven 7
-International because these games have different versions for different regions and thus the 
-additional info will not be deleted
+Upon visual inspection, some game names still contain additional information in parentheses. These 
+will be updated to remove the extra details, except for 'Dance Dance Revolution' and 'World Soccer 
+Winning Eleven 7 International', which have region-specific versions and will retain the additional 
+info.
 */
 UPDATE Video_Games 
 SET game_name = TRIM(SUBSTRING(game_name FROM 1 FOR POSITION('(' IN game_name) - 1))
@@ -415,25 +384,22 @@ AND game_name  NOT IN
 SELECT DISTINCT platform FROM Video_Games;
 SELECT DISTINCT year_of_release FROM Video_Games;
 /*
-There is one outlier in this dataset regarding the year of release. The game Imagine: Makeup Artist
-is the only game released in 2020 and no other game in this dataset has a year of release between
-2017 and 2020
+There is an outlier in the release year: 'Imagine: Makeup Artist' is the only game released in 2020, 
+with no other games in the dataset released between 2017 and 2020.
 */
 DELETE FROM Video_Games
 WHERE year_of_release = 2020;
 
-
 SELECT DISTINCT genre FROM Video_Games;
 SELECT DISTINCT publisher FROM Video_Games;
-/*
-Some games have unknown publishers which cannot be inferred and will be deleted
-*/
+--Some games have unknown publishers which cannot be inferred and will be deleted.
 DELETE FROM Video_Games
 WHERE publisher = 'Unknown';
 
-
 SELECT DISTINCT developer FROM Video_Games;
 SELECT DISTINCT rating FROM Video_Games;
+
+--NORMALIZATION
 /*
 The column developer has multiple values in a single cell, thus it must be reduced to 1NF. To do that,
 another table named Developers will be created with the columns game_name, platform and developers.
@@ -443,9 +409,9 @@ SELECT game_name,platform, TRIM(developer) AS developer
 FROM Video_Games;
 
 /*
-For some games, the developers' name have commas in them used before the suffixes "Inc", "Ltd" and 
-"LLC". These suffixes will be deleted from names only if they have a comma in front so that they don't
-interfere with the division of the cells with multiple developers.
+For some games, developer names have commas before the suffixes "Inc", "Ltd", and "LLC". These 
+suffixes will be removed if preceded by a comma to avoid issues when splitting cells with multiple 
+developers.
 */
 UPDATE Developers
 SET developer = 
@@ -465,51 +431,42 @@ OR developer LIKE '% Inc'
 OR developer LIKE '%, Lda';
 
 /*
-The following sequence of querries will take the first developer of the cells with multiple ones and 
-insert it on a new line along with the game name and platform of the original line. After that, the 
-first developer from the original cell will be deleted. This process will be done a second time
-because within this dataset the games can have a maximum of 3 developers.
+The following queries will extract the first developer from cells with multiple developers, insert 
+it into a new row with the game name and platform, and then delete it from the original cell. This 
+process will be repeated since games can have up to three developers in the dataset.
 */
-BEGIN TRANSACTION;
---Separating the first developer of the cells that have 3
 INSERT INTO Developers
 SELECT game_name, platform,
 TRIM(SUBSTRING(developer FROM 1 FOR POSITION(',' IN developer) - 1))
 FROM Developers
 WHERE developer LIKE '%,%,%';
 
---Deleting the first developer from the cells that have 3 of them
 UPDATE Developers
 SET developer = TRIM(SUBSTRING(developer FROM POSITION(',' IN developer) + 1 FOR LENGTH(developer)))
 WHERE developer LIKE '%,%,%';
 
 /*
-Due to deleting the first developers from the cells with 3 based on commas, a cell now start with
-"Inc," and another one starts with "Ltd.,". These prefixes will be deleted
+After deleting the first developers from cells containing three, some cells now start with "Inc," 
+and others with "Ltd." These prefixes will be removed.
 */
 UPDATE Developers
 SET developer = TRIM(SUBSTRING(developer FROM POSITION(',' IN developer) + 1 FOR LENGTH(developer)))
 WHERE developer LIKE 'Ltd.,%'
 OR developer LIKE 'Inc,%';
 
---Separating the first developer of the cells that have 2
 INSERT INTO Developers
 SELECT game_name, platform,
 TRIM(SUBSTRING(developer FROM 1 FOR POSITION(',' IN developer) - 1))
 FROM Developers
 WHERE developer LIKE '%,%';
 
---Deleting the first developer from the cells that have 2
 UPDATE Developers
 SET developer = TRIM(SUBSTRING(developer FROM POSITION(',' IN developer) + 1 FOR LENGTH(developer)))
 WHERE developer LIKE '%,%';
 
-COMMIT;
-
 /*
-In the original dataset there were games that had in the developer columns the name of the company
-with and without the suffixes like "Inc". By deleting those suffixes when seperating the developers
-duplicate rows might have been produced.
+The original dataset included developer names with and without suffixes like "Inc." Removing these 
+suffixes while separating developers may have resulted in duplicate rows.
 */
 DELETE FROM Developers
 WHERE CTID IN
@@ -524,14 +481,10 @@ ALTER TABLE Video_Games
 DROP COLUMN developer;
 
 /*
-The current video_games table has the genre and rating column which depends only on the game_name, 
-while the primary key is composed of both game_name and platform, thus it must be reduced to the 2NF.
+The current video_games table contains both sales data and game details, allowing it to be 
+represented as a join between separate tables for each type of information, so it must be brought to
+5NF.
 */
-
---RENORMALIZE, BUT KEEP THE 2NF-ING OF VIDEO_GAMES
---RECREATE THE WHOLE PROJECT
-
---2NF
 ALTER TABLE Video_Games RENAME TO Sales;
 
 CREATE TABLE Games AS
@@ -555,10 +508,6 @@ WHERE CTID IN
 	HAVING COUNT(*) > 1
 );
 
---3NF CHECK
---4NF CHECK
-
---Adding constraints
 ALTER TABLE Sales
 ADD COLUMN game_version_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY;
 
@@ -590,5 +539,3 @@ ADD CONSTRAINT Developers_primary_key PRIMARY KEY(game_name, platform, developer
 ALTER TABLE Developers
 ADD CONSTRAINT Developers_foreign_key FOREIGN KEY(game_version_id) 
 REFERENCES Sales(game_version_id);
-
-SELECT * FROM Developers;
